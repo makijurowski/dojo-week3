5e80261326bc9b4b791911944f5817f454d609bb
Maki Roggers <makiroggers@gmail.com>
2017-09-03 19:37:11 -0700

    Added first assignment.

---
+++ Python/StringsAndLists.py
@@ -0,0 +1,14 @@
+"""This document is a homework assignment for Coding Dojo."""
+
+def find_the_word(words, target):
+    """ Using the following input, find and return the index position
+        for the word 'day.'
+        INPUT: str, OUTPUT int. """
+    return words.find(target)
+
+
+
+
+
+RESULT = find_the_word("It's thanksgiving day. It's my birthday,too!", "day")
+print RESULT
